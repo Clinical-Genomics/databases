dd2a432f50e7b2e8010ea09d4a3b14c8a2ec632c
Rikard <rikard.erlandsson@scilifelab.se>
2014-12-01 15:28:06 +0100

    Update select_flowcellqc.py

--- scripts/select_flowcellqc.py
+++ scripts/select_flowcellqc.py
@@ -95,7 +95,7 @@ else:
   print "Found something"
   rows = cursor.fetchall()
   for row in rows:
-    print row[0], row[1], row[2], row[3], row[4], row[5]
+    print row[0], row[1], row[2], row[3], row[4], row[5], row[6]
 
 
 

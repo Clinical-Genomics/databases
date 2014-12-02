e20fb1bf95b570768010ee07d28c8dc2e8811193
Rikard <rikard.erlandsson@scilifelab.se>
2014-12-02 08:14:00 +0100

    Update select_flowcellqc.py

--- scripts/select_flowcellqc.py
+++ scripts/select_flowcellqc.py
@@ -66,7 +66,7 @@ else:
 
 cursor.execute(""" SELECT YEAR(rundate) AS year, MONTH(rundate) AS month, COUNT(DISTINCT datasource.datasource_id) AS runs, 
                    ROUND(SUM(readcounts)/2000000, 2) AS "mil reads", 
-                   ROUND(SUM(readcounts)/(2000000*COUNT(DISTINCT datasource.datasource_id)),1) AS "mil reads/fc lane"
+                   ROUND(SUM(readcounts)/(2000000*COUNT(DISTINCT datasource.datasource_id)),1) AS "mil reads/fc lane", GROUP_CONCAT(datasource.datasource_id)
                   FROM datasource 
                   LEFT JOIN flowcell ON datasource.datasource_id = flowcell.datasource_id 
                   LEFT JOIN unaligned ON unaligned.flowcell_id = flowcell.flowcell_id 

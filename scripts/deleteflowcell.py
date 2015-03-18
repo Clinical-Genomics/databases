69797babb01eb8da7907bbf575026aa446a31238
Rikard <rikard.erlandsson@scilifelab.se>
2015-03-18 10:06:05 +0100

    Update deleteflowcell.py

--- scripts/deleteflowcell.py
+++ scripts/deleteflowcell.py
@@ -18,9 +18,11 @@ if (len(sys.argv)>2):
   configfile = sys.argv[2]
   if not os.path.isfile(configfile):
     exit("Bad configfile")
+  fcname = sys.argv[1]
 else:
   if len(sys.argv) == 2:
     configfile = 'None'
+    fcname = sys.argv[1]
   else:
     print "usage: deleteflowcell.py <flowcellname> <config_file:optional>"
     exit(1)
@@ -39,6 +41,18 @@ with db.create_tunnel(pars['TUNNELCMD']):
     else:
       print "Correct db " + pars['STATSDB'] + " v:" + pars['DBVERSION']
 
-
-
+    print fcname
+    totalquery = """ SELECT project.projectname, flowcell.flowcellname, sample.samplename, unaligned.lane, 
+      unaligned.readcounts, unaligned.yield_mb, TRUNCATE(q30_bases_pct,2), TRUNCATE(mean_quality_score,2),
+      flowcell.flowcell_id, sample.sample_id, unaligned.unaligned_id, datasource.datasource_id, datasource.document_path,
+      supportparams.supportparams_id, project.project_id, supportparams.document_path
+      FROM sample, flowcell, unaligned, project, datasource, supportparams
+      WHERE sample.sample_id     = unaligned.sample_id
+      AND   flowcell.flowcell_id = unaligned.flowcell_id
+      AND   sample.project_id    = project.project_id 
+      AND   datasource.datasource_id = flowcell.datasource_id
+      AND   datasource.supportparams_id = supportparams.supportparams_id
+      AND   flowcellname = '""" + fcname + """'
+      ORDER BY flowcellname, sample.samplename, lane """
+    print totalquery
 

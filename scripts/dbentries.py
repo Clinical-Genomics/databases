d69a6a580833ca16c98806686f06e51da3a73d10
Rikard <rikard.erlandsson@scilifelab.se>
2015-03-25 14:06:31 +0100

    Update dbentries.py

--- scripts/dbentries.py
+++ scripts/dbentries.py
@@ -5,26 +5,24 @@ import os
 import time
 from access import db
 
-"""Removes flowcell/demux from db.
-  usage: deleteflowcell.py  <flowcellname> <config_file:optional>
-  Will list all demux from flowcell allowing user to chose which ones to delete
+"""Counts database entries.
+  usage: dbentries.py <config_file:optional>
+  Will list tables and count entries
 Args:
-  flowcellname (str): name of flowcell containing demux stats to delete
+  config_file (str): name of config file
 Returns:
-  Prints out all changes to the database
+  Counts all table entries
 """
 
-if (len(sys.argv)>2):
-  configfile = sys.argv[2]
+if (len(sys.argv)>1):
+  configfile = sys.argv[1]
   if not os.path.isfile(configfile):
     exit("Bad configfile")
-  fcname = sys.argv[1]
 else:
-  if len(sys.argv) == 2:
+  if len(sys.argv) == 1:
     configfile = 'None'
-    fcname = sys.argv[1]
   else:
-    print "usage: deleteflowcell.py <flowcellname> <config_file:optional>"
+    print "usage: dbentries.py <config_file:optional>"
     exit(1)
 pars = db.readconfig(configfile)
 
